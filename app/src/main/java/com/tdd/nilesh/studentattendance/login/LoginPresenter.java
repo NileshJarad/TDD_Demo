302962ea6f63aa3afba003efa043ebacbaf02345
NJ <nilworking@gmail.com>
2016-08-03 09:00:43 +0530

    Unit Test correction
    1) Corrected logic and test

--- app/src/main/java/com/tdd/nilesh/studentattendance/login/LoginPresenter.java
+++ app/src/main/java/com/tdd/nilesh/studentattendance/login/LoginPresenter.java
@@ -9,7 +9,17 @@ public class LoginPresenter {
     private static final int MAX_LOGIN_ATTEMPT = 3;
     private int loginAttempt;
 
+
+    public int incrementLoginAttempt() {
+        loginAttempt = loginAttempt + 1;
+        return loginAttempt;
+    }
+
     public boolean isLoginAttemptExceeded() {
-        return loginAttempt<MAX_LOGIN_ATTEMPT;
+        return loginAttempt >= MAX_LOGIN_ATTEMPT;
     }
 }
+
+
+
+
--- app/src/test/java/com/tdd/nilesh/studentattendance/login/LoginPresenterTest.java
+++ app/src/test/java/com/tdd/nilesh/studentattendance/login/LoginPresenterTest.java
@@ -11,6 +11,9 @@ public class LoginPresenterTest {
     @Test
     public void checkIfLoginAttemptIsExceeded() {
         LoginPresenter loginPresenter = new LoginPresenter();
+        Assert.assertEquals(1, loginPresenter.incrementLoginAttempt());
+        Assert.assertEquals(2, loginPresenter.incrementLoginAttempt());
+        Assert.assertEquals(3, loginPresenter.incrementLoginAttempt());
         Assert.assertTrue(loginPresenter.isLoginAttemptExceeded());
     }
 
