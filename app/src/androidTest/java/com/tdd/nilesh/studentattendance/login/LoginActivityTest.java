49d49c466361ef21dd51ec7a9b73179b6599a53a
preethambogadi <pbogadi93@gmail.com>
2017-09-09 11:59:51 -0400

    2nd commit to test in jenkins

--- app/src/androidTest/java/com/tdd/nilesh/studentattendance/login/LoginActivityTest.java
+++ app/src/androidTest/java/com/tdd/nilesh/studentattendance/login/LoginActivityTest.java
@@ -23,6 +23,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * Created by NJ on 24/08/16.
  */
 
+// just normal space
 //Follow this tutorial https://medium.com/mobility/how-to-do-tdd-in-android-90f013d91d7f
 @RunWith(AndroidJUnit4.class)
 public class LoginActivityTest {
