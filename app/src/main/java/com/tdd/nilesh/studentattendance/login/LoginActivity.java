a701d02c7d9d955294d210028837e2022be3be32
rezas <rezastallone@gmail.com>
2019-05-09 23:42:07 +0700

    Fix build error by updating dependencies

--- app/src/main/java/com/tdd/nilesh/studentattendance/login/LoginActivity.java
+++ app/src/main/java/com/tdd/nilesh/studentattendance/login/LoginActivity.java
@@ -1,12 +1,13 @@
 package com.tdd.nilesh.studentattendance.login;
 
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.snackbar.Snackbar;
 import com.tdd.nilesh.studentattendance.R;
 
 public class LoginActivity extends AppCompatActivity implements LoginView {
